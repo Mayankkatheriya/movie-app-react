5fc32556d75b571387daa3d3f018faef2e90807c
Mayankkatheriya <mayankkatheriya4@gmail.com>
2023-12-20 00:23:44 +0530

    like and share buttons active

--- src/Components/Main/Card.js
+++ src/Components/Main/Card.js
@@ -1,6 +1,34 @@
 import "./Card.css";
+import {useState} from 'react'
 
 const Card = (props) => {
+  let [heartStyle , setStyle] = useState({
+    color : "white"
+  })
+
+  const likeCheck = () => {
+    let updatedObj = {...heartStyle}
+    if(heartStyle.color === "white"){
+      updatedObj.color = "red"
+    }
+    else {
+      updatedObj.color = "white"
+
+    }
+    setStyle(updatedObj)
+  }
+
+  const shareText = async() => {
+    try {
+      await navigator.share({
+        title: `${props.item.title}`,
+        text: `I just read "${props.item.title}" on the Movie-Site App. It was really interesting!`,
+      });   
+    } catch (err) {
+      console.error("error:", err.message);
+    }
+  }
+
   return (
     <div
       className="card"
@@ -18,9 +46,9 @@ const Card = (props) => {
               <div className="text">{props.item.lang}</div>
               <div className="text">{props.item.desc}</div>
               <div className="card-icons">
-                <i className="bx bxs-share-alt"></i>
-                <i className="bx bxs-heart"></i>
-                <i className="bx bxs-message-square"></i>
+                <i title = "Share" onClick = {shareText} className="bx bxs-share-alt"></i>
+                <i title = "Like" onClick = {likeCheck} className="bx bxs-heart" style = {heartStyle}></i>
+                <i title = "Review" className="bx bxs-message-square"></i>
               </div>
             </div>
           </div>
